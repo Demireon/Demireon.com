0111993ff48fff170b1bae75e4d8e7b6053711b3
Ali Demirci <alidemir95@gmail.com>
2018-11-04 12:40:49 -0600

    Added icons for linkedin, medium, and email

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Icon from '../icon';
 import Github from '../icon/github.icon';
+import Linkedin from '../icon/linkedin.icon';
+import Medium from '../icon/medium.icon';
+import Email from '../icon/email.icon';
 import './_footer.scss';
 
 const Footer = () =>
@@ -21,10 +24,18 @@ const Footer = () =>
             <a href="https://github.com/Demireon" target="_blank" rel="noopener noreferrer" className="footer-icon">
               <Icon icon={Github} width={72} height={72} fill={'#fff'} />
             </a>
+            <a href="https://linkedin.com/in/demireon" target="_blank" rel="noopener noreferrer" className="footer-icon">
+              <Icon icon={Linkedin} width={72} height={72} fill={'#fff'} />
+            </a>
+            <a href="https://medium.com/@Demireon" target="_blank" rel="noopener noreferrer" className="footer-icon">
+              <Icon icon={Medium} width={72} height={72} fill={'#fff'} />
+            </a>
+            <a href="mailto:alidemir95@gmail.com" target="_blank" rel="noopener noreferrer" className="footer-icon">
+              <Icon icon={Email} width={72} height={72} fill={'#fff'} />
+            </a>
           </div>
         </div>
       </div>
-
     </div>
   </footer>);
 
