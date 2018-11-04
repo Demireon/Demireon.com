1e3047ab56aee165ac5c507e7fc7a2bd9695c11e
Ali Demirci <alidemir95@gmail.com>
2018-11-04 13:42:22 -0600

    Content and styling change of the intro section

--- src/components/features/index.js
+++ src/components/features/index.js
@@ -2,23 +2,14 @@ import React from 'react';
 
 import './_features.scss';
 
-const NewsListing = props =>
+const NewsListing = () =>
   (<section className="features" id="features">
     <div className="container">
       <div className="row">
         <div className="col-12">
-          <h4 className="features-title">Features</h4>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-12">
-          <ul className="features-list">
-            {props.data.map((item, i) =>
-              (<li className="features-list-item" key={i}>
-                {item.title}
-              </li>),
-            )}
-          </ul>
+          <span className="features-content">
+          Hi!
+          </span>
         </div>
       </div>
     </div>
