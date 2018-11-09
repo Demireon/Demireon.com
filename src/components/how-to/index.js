7535c916bf073c65358bd24f3e90557861416c1f
Ali Demirci <alidemir95@gmail.com>
2018-11-09 14:43:50 -0600

    changed the basic content and styling

--- src/components/how-to/index.js
+++ src/components/how-to/index.js
@@ -3,22 +3,11 @@ import React from 'react';
 import './_how-to.scss';
 
 const NewsListing = props =>
-  (<section className="howto" id="howto">
+  (<section className="work" id="work">
     <div className="container">
       <div className="row">
         <div className="col-12">
-          <h4 className="howto-title">How to</h4>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-12">
-          <ol className="howto-list">
-            {props.data.map((item, i) =>
-              (<li className="howto-list-item" key={i}>
-                {item.title}
-              </li>),
-            )}
-          </ol>
+          <h4 className="work-title">Sample work</h4>
         </div>
       </div>
     </div>
